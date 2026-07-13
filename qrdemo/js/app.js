dfa7943d70db1ba4c088fad34bb3794eeb66a5e9
nagesh <56336335+nagesh-repo@users.noreply.github.com>
2026-07-13 18:52:09 +0530

    Add files via upload
    Initial demo

---
+++ qrdemo/js/app.js
@@ -0,0 +1,31 @@
+const btnGenerate = document.getElementById("btnGenerate");
+
+btnGenerate.addEventListener("click", generateQR);
+
+function generateQR() {
+
+    const message = document.getElementById("txtMessage").value.trim();
+
+    if (message === "") {
+        alert("Please enter a message.");
+        return;
+    }
+
+    // CHANGE THIS AFTER GITHUB PAGES IS READY
+    const baseURL =
+        "https://YOUR_GITHUB_USERNAME.github.io/qrdemo/viewer.html";
+
+    const url =
+        baseURL +
+        "?msg=" +
+        encodeURIComponent(message);
+
+    document.getElementById("qrcode").innerHTML = "";
+
+    new QRCode(document.getElementById("qrcode"), {
+        text: url,
+        width: 250,
+        height: 250
+    });
+
+}
