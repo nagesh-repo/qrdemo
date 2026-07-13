720ef0359ced20143d62d6f60e21ff3c4eb7fbb5
nagesh <56336335+nagesh-repo@users.noreply.github.com>
2026-07-13 22:43:23 +0530

    Update app.js
    Version_2

--- app.js
+++ app.js
@@ -1,31 +1,106 @@
+/*
+======================================================
+
+QR Message Generator
+Version 2.0
+
+Responsibilities
+
+✔ Validate Message
+✔ Generate QR
+✔ Download QR Image
+
+======================================================
+*/
+
 const btnGenerate = document.getElementById("btnGenerate");
+const btnDownload = document.getElementById("btnDownload");
+
+const txtMessage = document.getElementById("txtMessage");
+const qrContainer = document.getElementById("qrcode");
+
+/*
+======================================================
+Generate QR
+======================================================
+*/
 
 btnGenerate.addEventListener("click", generateQR);
 
 function generateQR() {
 
-    const message = document.getElementById("txtMessage").value.trim();
+    const message = txtMessage.value.trim();
 
-    if (message === "") {
+    if (message.length === 0) {
+
         alert("Please enter a message.");
+
+        txtMessage.focus();
+
         return;
+
     }
 
-    // CHANGE THIS AFTER GITHUB PAGES IS READY
-const baseURL =
-    "https://nagesh-repo.github.io/qrdemo/Viewer.html";
+    /*
+    --------------------------------------
+    Change ONLY if GitHub repository changes
+    --------------------------------------
+    */
 
-    const url =
+    const baseURL =
+        "https://nagesh-repo.github.io/qrdemo/Viewer.html";
+
+    const finalURL =
         baseURL +
         "?msg=" +
         encodeURIComponent(message);
 
-    document.getElementById("qrcode").innerHTML = "";
+    qrContainer.innerHTML = "";
 
-    new QRCode(document.getElementById("qrcode"), {
-        text: url,
-        width: 250,
-        height: 250
+    new QRCode(qrContainer, {
+
+        text: finalURL,
+
+        width:250,
+
+        height:250
+
     });
+
+    btnDownload.style.display = "block";
+
+}
+
+/*
+======================================================
+Download QR
+======================================================
+*/
+
+btnDownload.addEventListener("click", downloadQR);
+
+function downloadQR(){
+
+    const qrImage = qrContainer.querySelector("img");
+
+    if(!qrImage){
+
+        alert("Please generate a QR Code first.");
+
+        return;
+
+    }
+
+    const link = document.createElement("a");
+
+    link.href = qrImage.src;
+
+    link.download = "QR_Message.png";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
 
 }
